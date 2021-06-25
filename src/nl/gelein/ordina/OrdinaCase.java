fb78a6bfc8f0a4714bcc0adc237363024b21a18c
Mees Gelein <meesgelein@gmail.com>
2021-06-25 15:50:47 +0200

    Updating a single linebreak

--- src/nl/gelein/ordina/OrdinaCase.java
+++ src/nl/gelein/ordina/OrdinaCase.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
+
 /**
  * A simple test case for the Ordina interview, requiring string manipulation.
  * @author Mees Gelein
